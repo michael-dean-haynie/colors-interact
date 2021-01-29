9e88b34f4666140d59cefe008a4fc21188a67ae1
Michael Dean Haynie <michael.dean.haynie@gmail.com>
2021-01-28 21:28:36 -0500

    first commit

---
+++ src/main/java/com/codetudes/colorsinteract/Greeting.java
@@ -0,0 +1,18 @@
+package com.codetudes.learnspringwebsockets;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
